604efb87dccc97aa09f058e42ff22a3071915963
John Mason <johnmason1701@gmail.com>
2025-11-24 09:43:26 +0700

    Add authentication through service account for more secure when deploying to GCP Cloud Run y

--- src/main/java/org/jarkata/gcp/helidongcp/HelloResource.java
+++ src/main/java/org/jarkata/gcp/helidongcp/HelloResource.java
@@ -9,6 +9,6 @@ public class HelloResource {
     @GET
     @Produces("text/plain")
     public String hello() {
-        return "Hello, World! xxx-yyy-zzz";
+        return "Hello, World! xxx-yyy-zzz-ggg";
     }
 }
